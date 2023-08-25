880a2b2efe5490a9196435e003c6a43bc7e6c803
jaceshi <jaceshi@tencent.com>
2023-08-25 18:12:20 +0800

    update lc150

---
+++ lc150/array-string/105.cpp
@@ -0,0 +1,29 @@
+// 14. 最长公共前缀
+#include "../def.h"
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) {
+            return "";
+        }
+        string prefix = strs[0];
+        for (int i=1; i<strs.size(); i++) {
+            prefix = commonPrefix(prefix, strs[i]);
+            if (prefix.empty()) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    string commonPrefix(string str1, string str2) {
+        int len = min(str1.size(), str2.size());
+        int i = 0;
+        while (i < len && str1[i] == str2[i]) {
+            i++;
+        }
+
+        return str1.substr(0, i);
+    }
+};
--- lc150/double-pointer/032.cpp
+++ lc150/double-pointer/032.cpp
@@ -1,3 +1,26 @@
 // 15. 三数之和
+#include "../def.h"
 
 // 排序+双指针
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end(), less<int>());
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    if ((nums[i] + nums[j] + nums[k]) == 0) {
+                        res.push_back({nums[i], nums[j], nums[k]});
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
